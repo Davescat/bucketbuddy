9a8f87f9361e9d0d9bc69ea5340a01c411a9b96e
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-08-17 21:57:07 -0400

    Removed test path in router.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'semantic-ui-css/semantic.min.css';
 import './App.scss';
 import BucketViewer from './components/modules/BucketViewer';
 import ConnectToS3Bucket from './pages/ConnectToS3Bucket';
-import SchemaForm from './components/schema-form';
 
 function App() {
   return (
@@ -12,8 +11,6 @@ function App() {
       <Switch>
         <Route exact path="/" component={ConnectToS3Bucket} />
         <Route path="/bucket-viewer" component={BucketViewer} />
-        {/* TODO this is to test the schema form and is to be removed */}
-        <Route exact path="/test" component={SchemaForm} />
       </Switch>
     </Router>
   );
