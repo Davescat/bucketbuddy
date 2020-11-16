e961e94a73bdd456628143e16e8451e5732e2879
Davescat <47789084+Davescat@users.noreply.github.com>
2020-11-15 23:38:49 -0500

    Introduced Custum theming.
    Before running "npm start" please do "npm build-semantic". This will build the css file to use accross the app. Might remove in the future and just keep the generated css min file that is left, but for now will be used to tinker with styles and custom themes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
+import './fomantic/dist/semantic.min.css';
 import './App.scss';
 import BucketViewer from './components/modules/BucketViewer/index';
 import ConnectToS3Bucket from './pages/ConnectToS3Bucket';
